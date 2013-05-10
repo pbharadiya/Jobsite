180fa0afa777080da2734572898b03db4f86742c
nikhil yeole <nikhily@cybage.com>
2013-05-10 18:44:03 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jobsite::Application.config.secret_token = '0568166f480cb563bc3cac1016e048af6d9766ac4ca1ef32f8d69889e51413c2d28d078873bb46eb972b2d4ad0df23145b11770be9332869bdb16f732c3c06d2'
